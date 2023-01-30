556c34c357eda86dc179ae7946dc94711dcdb68b
skhrlx <85849948+skhrlx@users.noreply.github.com>
2023-01-30 20:12:27 -0300

    Update Trident.ino

--- Trident/Trident.ino
+++ Trident/Trident.ino
@@ -1,7 +1,11 @@
 #include <Keyboard.h>
 #include <Mouse.h>
 
-char incomingByte;
+#define CMD_JUMP   1  // No arguments
+#define CMD_SHOOT  2  // No arguments
+#define CMD_AIM    3  // x, y -> how much to move mouse to in X and Y axis
+#define CMD_RCS    4  // n - number of NOT dormant enemies (hp, weapon, position)
+#define CMD_AFK    5
 
 void setup() {
   delay(2000);
@@ -9,70 +13,28 @@ void setup() {
   delay(2000);
   Serial.begin(128000);
 }
-void loop(){
-  
-  if(Serial.available()>0){
-    incomingByte = Serial.read();
-    if(incomingByte == 'l'){
-      gc_bhop();
+void loop() {
+  if (Serial.available()) {
+    int cmd = Serial.read();
+    switch (cmd) {
+      case CMD_JUMP: {
+        Mouse.move(0, 0, -10);
+      }
+      break;
+      case CMD_SHOOT: {
+        Mouse.click(MOUSE_LEFT);
+      }
+      break;
+      case CMD_RCS: {
+        Mouse.move(-1, 2, 0);
+      }
+      break;
+      case CMD_AIM: {
+        char delta_x = static_cast<char>(Serial.read());
+        char delta_y = static_cast<char>(Serial.read());
+        Mouse.move(delta_x, delta_y, 0);
+        }
+      break;
     }
-    
-    if(incomingByte == 'o'){
-      mm_bhop();
-    }
-
-    if(incomingByte == 'k'){
-      rcs();
-    }
-
-    if(incomingByte == 'i'){
-      valorant_bhop();
-    }
-
-    if(incomingByte == 'j'){
-      shoot_kb();
-    }
-
-    if(incomingByte == 'm'){
-      left_ac();
-    }
-
-    if(incomingByte == 'n'){
-      left_ac();
-    }
-    
   }
-}
-
-
-void mm_bhop(){
-  Mouse.move(0, 0, 1);
-}
-
-void rcs(){
-  Mouse.move(0, 1, 0);
-}
-
-void gc_bhop(){
-  Keyboard.write('l');
-  Keyboard.write('o');
-  Keyboard.write('k');
-  Keyboard.write('i');
- }
-
- 
-void shoot_kb(){
-  Keyboard.write('l');
-}
-
-void valorant_bhop(){
-  Keyboard.write('o');
-}
-
-void left_ac(){
-  Mouse.click(MOUSE_LEFT);
-}
-
-void right_ac(){
-  Mouse.click(MOUSE_RIGHT);
 }
