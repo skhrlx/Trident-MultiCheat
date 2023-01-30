bb5e060e4cb7200c6a006753fa06aac3f010b567
skhrlx <85849948+skhrlx@users.noreply.github.com>
2023-01-30 20:11:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,25 +12,44 @@ import numpy as np
 arduino = serial.Serial('COM5', 128000)
 
 GC = False
-MM = True
+MM = False
 VALORANT = False
 RCS = True
 TRIGGERBOT = True
 MINECRAFT = False
+AIMBOT = True
+
 S_HEIGHT, S_WIDTH  = ImageGrab.grab().size
 GRABZONE           = 3
- 
-class AimBot:
-    def __init__(self, fov=10):
-        self.fov = fov
-        self.sct = mss()
-        self.screenshot = self.sct.monitors[1]
-        self.screenshot['left'] = int((self.screenshot['width'] / 2) - (self.fov / 2))
-        self.screenshot['top'] = int((self.screenshot['height'] / 2) - (self.fov / 2))
-        self.screenshot['width'] = self.fov
-        self.screenshot['height'] = self.fov
-        self.center = self.fov / 2
 
+def gc_bhop(): 
+    return arduino.write(b'l')
+    
+def mm_bhop():
+    return arduino.write(b'o')
+
+def rcs():
+    pax = [int(4)]
+    return arduino.write(pax)
+    
+def valorant_bhop():
+    return arduino.write(b'i')
+
+def shoot_kb(): 
+    pax = [int(2)]
+    return arduino.write(pax)
+
+def left_ac(): 
+    return arduino.write(b'm')
+
+def right_ac(): 
+    return arduino.write(b'n')
+
+class FoundEnemy(Exception):
+    pass
+
+class AimBot:
+    def __init__(self, fov=20):
         self.lower = np.array([140, 111, 160])
         self.upper = np.array([148, 154, 194])
 
@@ -44,56 +63,44 @@ class AimBot:
         if y < 0:
             y = y + 256
 
-        pax = [int(x), int(y)]
+        pax = [int(3), int(x), int(y)]
         arduino.write(pax)
 
+    def grab(self):
+        self.sct = mss()
+        self.fov = 10
+        self.screenshot = self.sct.monitors[1]
+        self.screenshot['left'] = int((self.screenshot['width'] / 2) - (self.fov / 2))
+        self.screenshot['top'] = int((self.screenshot['height'] / 2) - (self.fov / 2))
+        self.screenshot['width'] = self.fov
+        self.screenshot['height'] = self.fov
+        self.center = self.fov / 2
+        with mss() as sct:
+            return np.array(self.sct.grab(self.screenshot))
+
+         
     def run(self):
-                img = np.array(self.sct.grab(self.screenshot))
-                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-                mask = cv2.inRange(hsv, self.lower, self.upper)
-                kernel = np.ones((3, 3), np.uint8)
-                dilated = cv2.dilate(mask, kernel, iterations=5)
-                thresh = cv2.threshold(dilated, 60, 255, cv2.THRESH_BINARY)[1]
-                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-                if len(contours) != 0:
-                    M = cv2.moments(thresh)
-                    point_to_aim = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        img = self.grab()
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, self.lower, self.upper)
+        kernel = np.ones((3, 3), np.uint8)
+        dilated = cv2.dilate(mask, kernel, iterations=5)
+        thresh = cv2.threshold(dilated, 60, 255, cv2.THRESH_BINARY)[1]
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) != 0:
+            M = cv2.moments(thresh)
+            point_to_aim = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-                    closestX = point_to_aim[0] + 2
-                    closestY = point_to_aim[1] - 5
+            closestX = point_to_aim[0] + 2
+            closestY = point_to_aim[1] - 5
 
-                    diff_x = int(closestX - self.center)
-                    diff_y = int(closestY - self.center)
+            diff_x = int(closestX - self.center)
+            diff_y = int(closestY - self.center)
 
-                    target_x = diff_x * self.xspd
-                    target_y = diff_y * self.yspd
+            target_x = diff_x * self.xspd
+            target_y = diff_y * self.yspd
 
-                    self.mousemove(target_x, target_y)
-
-def gc_bhop(): 
-    return arduino.write(b'l')
-    
-def mm_bhop():
-    return arduino.write(b'o')
-
-def rcs():
-    return arduino.write(b'k')
-    
-def valorant_bhop():
-    return arduino.write(b'i')
-
-def shoot_kb(): 
-    return arduino.write(b'j')
-
-def left_ac(): 
-    return arduino.write(b'm')
-
-def right_ac(): 
-    return arduino.write(b'n')
-
-class FoundEnemy(Exception):
-    pass
-
+            self.mousemove(target_x, target_y)
 
 class TriggerBot:
 
@@ -127,10 +134,15 @@ if __name__ == "__main__":
     _hash = sha256(f'{random.random()}'.encode('utf-8')).hexdigest()
     print(_hash), system(f'title {_hash}'), sleep(0.5), system('@echo off'), system('cls')
     bot = TriggerBot()
+    aim = AimBot()
     while True:
         
         if win32api.GetAsyncKeyState(0x20) and GC:
             gc_bhop()
+            continue
+
+        if win32api.GetAsyncKeyState(0x01) and AIMBOT:
+            aim.run()
             continue
         
         if win32api.GetAsyncKeyState(0x20) and MM:
